40b94d0356d9dfb367b55d5c80af609d36e56cbd
Оборожный Данил <Middle-Shell@users.noreply.github.com>
2020-10-02 17:06:47 +0300

    Function

---
+++ Python/series21+function.py
@@ -0,0 +1,10 @@
+def uneven():
+	a = list(map(int, input("Enter through space N numbers ").split()))
+	k = True
+	for i in range(len(a)-1):
+		if a[i+1] <= a[i]:
+			k = False
+			break
+	print(k)
+	
+uneven()
