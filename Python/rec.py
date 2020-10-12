2b414fc397f0da7215f285971e9eff6fe2f2b31d
Оборожный Данил <72213265+Middle-Shell@users.noreply.github.com>
2020-10-12 13:20:11 +0300

    recursion
    12.10.20

---
+++ Python/rec.py
@@ -0,0 +1,9 @@
+arr = [1, 2, [3, 4, [1, 2, 3, 4,[1, 2, 3], 5, 6], 7, 8], 9, 9]
+
+def rec(mas):
+    for i in range(len(mas)):
+        if type(mas[i]) is int:
+            print(mas[i])
+        else:
+            rec(mas[i])
+rec(arr)
