91e0d49342b9a72b9e516e5e7253a38bad2f5cde
Оборожный Данил <72213265+Middle-Shell@users.noreply.github.com>
2020-10-25 10:43:11 +0300

    Update rec_pyFiles.py

--- Python/rec_pyFiles.py
+++ Python/rec_pyFiles.py
@@ -1,14 +1,20 @@
 import os
 import datetime
 
-def rec(dir):
+def rec(dir, mdir):
     for (dirpath, dirnames, files) in os.walk(dir):
+        try:
+            if mdir != dirpath.split('\\')[4]:
+                mdir = dirpath.split('\\')[4]
+                print(mdir)
+        except:
+            pass
         for file in files:
             if file.endswith('.py'):
-                print(file)
-                print(datetime.datetime.fromtimestamp(os.path.getmtime(dir + '/' + file)))
+                print('   ' + file)
+                print('   DataMod: ' + str(datetime.datetime.fromtimestamp(os.path.getmtime(dir + '\\' + file))))
         if len(dirnames) != 0:
             for i in range(len(dirnames)):
-                rec(dir + '//' + dirnames[i])
+                rec(dir + '\\' + dirnames[i], mdir)
         return 0
-rec("1kurs")
+rec("C:\\Users\\79101\\Desktop", '')
